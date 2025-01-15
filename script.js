92b2efdf171f7df6344796a7498ff1ea1f4cd44e
Breno <28717383+brenobmac@users.noreply.github.com>
2025-01-15 16:25:17 -0300

    Added function for round playing, which will check if the player wins or if computer wins or if it's a tie.

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let choice = prompt("Choose between rock, paper or scissors!");
-  return choice.toLowerCase(); // For easy comparison
+  let choice = prompt("Choose between rock, paper or scissors!").toLowerCase();
+
+  return choice; // For easy comparison
+}
+
+function playRound(player, computer) {
+  let wins = { rock: "scissors", paper: "rock", scissors: "paper" };
+
+  if (wins[player] == computer) {
+    console.log(`Player wins - ${player} beats ${computer}!`);
+    return 0;
+  } else if (wins[computer] == player) {
+    console.log(`Computer wins - ${computer} beats ${player}!`);
+    return 1;
+  } else {
+    console.log("It's a tie!");
+    return 2;
+  }
 }
