9b580d0cf51e6f7f530361fa5b565a1181b91abb
Breno <28717383+brenobmac@users.noreply.github.com>
2025-01-22 09:13:14 -0300

    Added HTML portions for player score and computer score. Set up functions to start and reset button functionality (needed to remove arrow functions and add new separate functions for the buttons, because we can't remove arrow functions from buttons).

--- script.js
+++ script.js
@@ -21,47 +21,43 @@ function playRound(player, computer) {
     }
 }
 
-/*
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let rounds = Number(prompt("How many rounds would you like to play?"));
-
-    // Will keep running until rounds reach 0.
-    while (rounds) {
-        let computerChoice = getComputerChoice();
-        let playerChoice = getPlayerChoice();
-        let result = playRound(playerChoice, computerChoice);
-
-        if (result == 0) {
-            playerScore += 1;
-        } else if (result == 1) {
-            computerScore += 1;
-        } else {
-        }
-
-        rounds -= 1;
-    }
-
-    console.log(
-        `Game over!! Player score - ${playerScore}. Computer score - ${computerScore}.`
-    );
+// Functions to call playRound
+// We need to define them because we want to remove them from the buttons later, since
+// we can't remove arrow functions directly from the buttons
+function rockClick() {
+    playRound("rock", getComputerChoice()); //Plays round with rock and computer choice.
 }
-*/
+
+function paperClick() {
+    playRound("paper", getComputerChoice()); //Plays round with paper and computer choice.
+}
+
+function scissorClick() {
+    playRound("scissors", getComputerChoice()); //Plays round with scissors and computer choice.
+}
 
 // Setting up the buttons to play the game instead of old getPlayerChoice function.
+function buttonStart() {
+    let rockButton = document.querySelector("#rock-button");
+    rockButton.addEventListener("click", rockClick);
 
-let rockButton = document.querySelector("#rock-button");
-rockButton.addEventListener("click", () => {
-    playRound("rock", getComputerChoice()); //Plays round with rock and computer choice.
-});
+    let paperButton = document.querySelector("#paper-button");
+    paperButton.addEventListener("click", paperClick);
 
-let paperButton = document.querySelector("#paper-button");
-paperButton.addEventListener("click", () => {
-    playRound("paper", getComputerChoice()); //Plays round with paper and computer choice.
-});
+    let scissorButton = document.querySelector("#scissor-button");
+    scissorButton.addEventListener("click", scissorClick);
+}
 
-let scissorButton = document.querySelector("#scissor-button");
-scissorButton.addEventListener("click", () => {
-    playRound("scissors", getComputerChoice()); //Plays round with scissors and computer choice.
-});
+// Reset buttons wheh max score for either player is reached.
+function buttonReset() {
+    let rockButton = document.querySelector("#rock-button");
+    rockButton.removeEventListener("click", rockClick);
+
+    let paperButton = document.querySelector("#paper-button");
+    paperButton.removeEventListener("click", paperClick);
+
+    let scissorButton = document.querySelector("#scissor-button");
+    scissorButton.removeEventListener("click", scissorClick);
+}
+
+buttonStart();
